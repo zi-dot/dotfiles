ea98e49cbce5ab93f047a9ba75b2cdf29540bfd5
iz <k.ryoji.74@gmail.com>
2021-04-20 21:52:48 +0900

    add zprezto

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ln -sf ~/dotfiles/.zprezto/runcoms/zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zprezto/runcoms/zshenv ~/.zshenv
+ln -sf ~/dotfiles/.zprezto/runcoms/zprofile ~/.zprofile
+ln -sf ~/dotfiles/.zprezto/runcoms/zpreztorc ~/.zpreztorc
+ln -sf ~/dotfiles/.zprezto/runcoms/zlogout ~/.zlogout
+ln -sf ~/dotfiles/.zprezto/runcoms/zlogin ~/.zlogin
