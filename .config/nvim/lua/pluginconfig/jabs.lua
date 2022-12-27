b29c5b84dc70850a21806985d621717a8393d1d3
Ryoji Kono <k.ryoji.74@gmail.com>
2022-12-27 13:44:19 +0900

    Add jabs settings

--- .config/nvim/lua/pluginconfig/jabs.lua
+++ .config/nvim/lua/pluginconfig/jabs.lua
@@ -1,4 +1,9 @@
 require("jabs").setup({
+  position = "center",
+  width = 100,
+  height = 40,
+  border = "rounded",
   sort_mru = true,
+  use_devicons = true,
 })
 vim.keymap.set("n", "<leader>o", ":JABSOpen<CR>", { noremap = true })
