6a1333452078221905cd93b1aeea6633d96496a0
Ryoji Kono <k.ryoji.74@gmail.com>
2023-01-07 09:05:52 +0900

    🔧 Add lockfile

--- .config/nvim/lua/pluginconfig/null-ls.lua
+++ .config/nvim/lua/pluginconfig/null-ls.lua
@@ -19,13 +19,13 @@ local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
 
 null_ls.setup({
   sources = {
-    null_ls.builtins.diagnostics.eslint.with({
+    null_ls.builtins.diagnostics.eslint_d.with({
       diagnostics_format = "[eslint] #{m}\n(#{c})",
     }),
     null_ls.builtins.diagnostics.fish,
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.prettier,
-    null_ls.builtins.formatting.eslint,
+    null_ls.builtins.formatting.eslint_d,
   },
   on_attach = function(client, bufnr)
     if client.server_capabilities.documentFormattingProvider then
